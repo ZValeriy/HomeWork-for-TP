6675c97397f9079d44382a72d95e15070dc352b9
ZubairovValeriy <ValeriyIU5-12@yandex.ru>
2017-12-14 19:29:49 +0300

    Jackson instead of Json, try-with-resources

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,95 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
     static class Point {
-        int x;
-        int y;
 
-        public Point(int x, int y){
-            this.x = x;
-            this.y = y;
+
+        private int x;
+        private int y;
+
+        public Point(Point p1, Point p2) {
+            this.setX(p1.getX() + p2.getX());
+            this.setY(p2.getY() + p2.getY());
         }
 
+        public Point() {
+            this.setX(0);
+            this.setY(0);
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
     }
 
-    public static String readJson(String filepath) throws FileNotFoundException, IOException{
-            BufferedReader reader = new BufferedReader( new FileReader (filepath));
-            String line = null;
-            StringBuilder stringBuilder = new StringBuilder();
-            String ls = System.getProperty("line.separator");
-            while( ( line = reader.readLine() ) != null ) {
-                stringBuilder.append( line );
-                stringBuilder.append( ls );
+    public static String readJson(String filepath) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
             }
-            stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-            return stringBuilder.toString();
+        return stringBuilder.toString();
     }
+
+    public static Point makePoint(String json) {
+        Point point = new Point();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            point = mapper.readValue(json, Point.class);
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+        return point;
+    }
+
     public static void main(String[] args) {
         String path1 = "a.json";
         String path2 = "b.json";
 
-        try {
-            String a_json = readJson(path1);
+        String js1;
+        js1 = readJson(path1);
 
-            Gson gson1 = new Gson();
-            Point point1 = gson1.fromJson(a_json, Point.class);
+        String js2;
+        js2 = readJson(path2);
 
-            String b_json = readJson(path2);
+        ObjectMapper mapper = new ObjectMapper();
 
-            Gson gson2 = new Gson();
-            Point point2 = gson2.fromJson(b_json, Point.class);
+        Point point1 = new Point();
+        point1 = makePoint(js1);
 
-            Point new_point = new Point(point1.x +point2.x, point1.y + point2.y);
+        Point point2 = new Point();
+        point2 = makePoint(js2);
 
 
-            try (Writer writer = new FileWriter("c.json")) {
-                Gson gson = new GsonBuilder().create();
-                gson.toJson(new_point, writer);
-            }
-
-        } catch (IOException ex) {
-            System.out.println(ex.getStackTrace());
+        Point point3 = new Point(point1, point2);
+        try (FileOutputStream fout = new FileOutputStream("c.json")){
+            mapper.writeValue(fout, point3);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
